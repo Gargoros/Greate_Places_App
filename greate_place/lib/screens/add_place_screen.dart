c5eeb8812c717c241b53e8faf323108bae9cd2b3
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-11-01 02:07:16 +0300

    feat: init elevetedButton and some style for it

--- greate_place/lib/screens/add_place_screen.dart
+++ greate_place/lib/screens/add_place_screen.dart
@@ -9,6 +9,14 @@ class AddPlaceScreen extends StatefulWidget {
 }
 
 class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  final _titleController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,10 +24,42 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
         title: Text("Add a New place"),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Text("User inputs ..."),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: <Widget>[
+                    TextField(
+                      decoration: InputDecoration(labelText: "Title"),
+                      controller: _titleController,
+                    ),
+                    TextField(),
+                    TextField(),
+                  ],
+                ),
+              ),
+            ),
+          ),
           ElevatedButton.icon(
-              onPressed: () {}, icon: Icon(Icons.add), label: Text("Add Place"))
+              onPressed: () {},
+              icon: Icon(
+                Icons.add,
+                color: Colors.black,
+              ),
+              label: Text(
+                "Add Place",
+                style: TextStyle(color: Colors.black),
+              ),
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Theme.of(context).colorScheme.secondary,
+                  elevation: 0,
+                  tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                  shape:
+                      RoundedRectangleBorder(borderRadius: BorderRadius.zero)))
         ],
       ),
     );
