ac744c009369d500cb62b8cdea595c2e9b912731
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-11-08 21:33:11 +0300

    feat: init new consumer to show image list on screen

--- greate_place/lib/screens/places_list_screen.dart
+++ greate_place/lib/screens/places_list_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:greate_place/providers/great_places.dart';
 import 'package:greate_place/screens/add_place_screen.dart';
+import 'package:provider/provider.dart';
 
 class PlacesListScreen extends StatelessWidget {
   const PlacesListScreen({super.key});
@@ -17,8 +19,22 @@ class PlacesListScreen extends StatelessWidget {
                 icon: Icon(Icons.add))
           ],
         ),
-        body: Center(
-          child: CircularProgressIndicator(),
-        ));
+        body: Consumer<GreatPlaces>(
+            child: Center(
+              child: Text("Got no places yet, start adding some!"),
+            ),
+            builder: (context, greatPlace, ch) => greatPlace.items.length <= 0
+                ? ch!
+                : ListView.builder(
+                    itemCount: greatPlace.items.length,
+                    itemBuilder: (context, index) => ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage:
+                            FileImage(greatPlace.items[index].image),
+                      ),
+                      title: Text(greatPlace.items[index].title),
+                      onTap: () {},
+                    ),
+                  )));
   }
 }
