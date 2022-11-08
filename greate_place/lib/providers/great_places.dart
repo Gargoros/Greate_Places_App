3b3721d1c07cc704a77b9244c07247e1e1cc69d0
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-11-09 00:22:17 +0300

    feat: init method to work with dataBase

--- greate_place/lib/providers/great_places.dart
+++ greate_place/lib/providers/great_places.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
 import "package:flutter/foundation.dart";
+import 'package:flutter/material.dart';
+import 'package:greate_place/helpers/db_helper.dart';
 import '../models/place.dart';
 
 class GreatPlaces with ChangeNotifier {
@@ -17,6 +19,24 @@ class GreatPlaces with ChangeNotifier {
         location: null,
         image: pickedImage);
     _items.add(newPlace);
+    notifyListeners();
+
+    DBHelper.insert("user_places", {
+      "id": newPlace.id,
+      "title": newPlace.title,
+      "image": newPlace.image.path
+    });
+  }
+
+  Future<void> fetchAndSetPlaces() async {
+    final dataList = await DBHelper.getData("user_places");
+    dataList
+        .map((item) => Place(
+            id: item["id"],
+            title: item["title"],
+            location: null,
+            image: File(item["image"])))
+        .toList();
     notifyListeners();
   }
 }
