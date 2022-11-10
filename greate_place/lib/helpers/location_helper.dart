a893e464d7a5e13956300e056f06ccf0b83f4b91
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-11-10 22:11:14 +0300

    refactor: delete commit line

--- greate_place/lib/helpers/location_helper.dart
+++ greate_place/lib/helpers/location_helper.dart
@@ -8,5 +8,3 @@ class LocationHelper {
     return 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/pin-l($longitude,$latitude)/$longitude,$latitude,14.25,0,0/600x300?access_token=$MAPBOX_API_KEY';
   }
 }
-
-// https://api.mapbox.com/styles/v1/mikhailzhacko/claa8b2e400ka15phov7spmej.html?title=view&access_token=pk.eyJ1IjoibWlraGFpbHpoYWNrbyIsImEiOiJjbGFhNjlkMDkwNHNtM3VwbDIzcndzZW96In0.IVRy7GETDisJzYXUNkT4wg&zoomwheel=true&fresh=true#2/37.75/-92.25
