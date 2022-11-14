ada8e6981d678991ec227301b0553b5163edcf30
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2022-11-14 17:46:51 +0300

    feat: init new navigation logic

--- greate_place/lib/main.dart
+++ greate_place/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:greate_place/screens/add_place_screen.dart';
+import 'package:greate_place/screens/place_detail_screen.dart';
 import 'package:provider/provider.dart';
 import './providers/great_places.dart';
 import './screens/places_list_screen.dart';
@@ -27,6 +28,7 @@ class MyApp extends StatelessWidget {
         home: PlacesListScreen(),
         routes: {
           AddPlaceScreen.routeName: (context) => AddPlaceScreen(),
+          PlaceDetailScreen.routeName: (context) => PlaceDetailScreen(),
         },
       ),
     );
